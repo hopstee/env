28e4449589b388d720dcb4358cd9e81cfe72774b
Edik <hopsteee@gmail.com>
2025-01-28 22:07:37 +0300

    Пофиксил ошибки с параметрами key при рендеринге элементов в цикле

--- resources/js/Components/Breadcrumb.tsx
+++ resources/js/Components/Breadcrumb.tsx
@@ -11,24 +11,22 @@ export default function WorkspaceBreadcrumb() {
                 {breadcrumbs.map((breadcrumb: IBreadcrumb, index: number) => {
                     const showSeparator = index !== breadcrumbs.length - 1;
 
-                    return (
-                        <>
-                            <BreadcrumbItem key={index}>
-                                <BreadcrumbPage>
-                                    {breadcrumb.url
-                                        ? <Link
-                                            href={breadcrumb.url}
-                                            className="transition-colors text-zinc-500 dark:text-zinc-400 hover:text-zinc-950 dark:hover:text-zinc-50"
-                                        >
-                                            {breadcrumb.name}
-                                        </Link>
-                                        : breadcrumb.name
-                                    }
-                                </BreadcrumbPage>
-                            </BreadcrumbItem>
-                            {showSeparator && <BreadcrumbSeparator />}
-                        </>
-                    )
+                    return [
+                        <BreadcrumbItem key={`item-${index}`}>
+                            <BreadcrumbPage>
+                                {breadcrumb.url
+                                    ? <Link
+                                        href={breadcrumb.url}
+                                        className="transition-colors text-zinc-500 dark:text-zinc-400 hover:text-zinc-950 dark:hover:text-zinc-50"
+                                    >
+                                        {breadcrumb.name}
+                                    </Link>
+                                    : breadcrumb.name
+                                }
+                            </BreadcrumbPage>
+                        </BreadcrumbItem>,
+                        showSeparator && <BreadcrumbSeparator key={`separator-${index}`} />
+                    ]
                 })}
             </BreadcrumbList>
         </Breadcrumb>
